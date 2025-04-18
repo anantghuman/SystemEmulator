1c653c225d1ae8df4f829f7ff71fbd5452a9aca1
anantghuman <anant.ghuman@gmail.com>
2025-04-17 19:33:45 -0500

    fix

--- src/pipe/forward.c
+++ src/pipe/forward.c
@@ -19,21 +19,19 @@ comb_logic_t forward_reg(uint8_t D_src1, uint8_t D_src2, uint8_t X_dst,
                          bool W_wval_sel, bool X_w_enable, bool M_w_enable,
                          bool W_w_enable, uint64_t *val_a, uint64_t *val_b) {
     // STUDENT TODO
-    if ((D_src1 == X_dst) && X_w_enable) {
+    if (D_src1 == X_dst && X_w_enable) {
         *val_a = X_val_ex;
-    } else if ((D_src1 == M_dst) && M_w_enable) {
+    } else if (D_src1 == M_dst && M_w_enable) {
         *val_a = M_wval_sel ? M_val_mem : M_val_ex;
-    } else if ((D_src1 == W_dst) && W_w_enable) {
+    } else if (D_src1 == W_dst && W_w_enable) {
         *val_a = W_wval_sel ? W_val_mem : W_val_ex;
     }
-    // If none of the hazards apply, *val_a remains unchanged.
 
-    // Forward for operand B (associated with D_src2):
-    if ((D_src2 == X_dst) && X_w_enable) {
+    if (D_src2 == X_dst && X_w_enable) {
         *val_b = X_val_ex;
-    } else if ((D_src2 == M_dst) && M_w_enable) {
+    } else if (D_src2 == M_dst && M_w_enable) {
         *val_b = M_wval_sel ? M_val_mem : M_val_ex;
-    } else if ((D_src2 == W_dst) && W_w_enable) {
+    } else if (D_src2 == W_dst && W_w_enable) {
         *val_b = W_wval_sel ? W_val_mem : W_val_ex;
     }
 }
